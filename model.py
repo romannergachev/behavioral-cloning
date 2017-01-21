e54cc2f9fcc00d470b3e2a5166868c6ed3114767
Roman <nergachev.roman@gmail.com>
2017-01-22 00:25:23 +0200

    Merge branch 'master' of https://github.com/romannergachev/behavioral-cloning
    # Conflicts:
    #	model.py

--- model.py
+++ model.py
@@ -3,7 +3,15 @@ import math
 import csv
 import cv2
 import json
+import util
 
+from keras.applications import ResNet50
+from keras.applications import VGG16
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.engine import Input
+from keras.engine import Model
+from keras.layers import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
 from sklearn.utils import shuffle
 from keras.models import Sequential, model_from_json
 from keras.layers.core import Dense, Flatten, Dropout, Lambda
@@ -13,57 +21,53 @@ from keras.regularizers import l2
 from keras.optimizers import Adam
 import tensorflow as tf
 
-EPOCHS = 5
+EPOCHS = 20
 BATCH = 100
 WIDTH = 160
+# WiDTH = 100
+# HEIGHTS = 50
 HEIGHTS = 80
 CHANNEL_NUMBER = 3
-REG = 0.01
-FINE_TUNING = True
+REG = 0.0
+FINE_TUNING = False
+
+CROPPING = (0, 0, 0, 0)
 
 SHAPE = (HEIGHTS, WIDTH, CHANNEL_NUMBER)
 
 INITIAL_ITERATION = 'data/'
 FIRST_ITERATION = 'driving/'
 SECOND_ITERATION = 'driving2/'
-THIRD_ITERATION = 'driving3/'
+THIRD_ITERATION = 'not/track1_recovery/'
 ERRORS_ITERATION = 'errors/'
 
-SELECTED_FOLDER = THIRD_ITERATION
+SELECTED_FOLDER = INITIAL_ITERATION
 
 tf.python.control_flow_ops = tf
 
 
 def learning_rate():
     if FINE_TUNING:
-        return 0.00001
+        return 0.0001
     else:
-        return 0.001
+        return 0.0001
 
 
-def apply_clahe2(filename, mirrored=False):
-    image = cv2.imread(SELECTED_FOLDER + filename)
-    image = cv2.resize(image, (WIDTH, HEIGHTS))
-    if mirrored:
-        image = cv2.flip(image, 1)
-    img = image
+def crop_image(img, cropping):
+    return img[cropping[0]:img.shape[0] - cropping[1], cropping[2]:img.shape[1] - cropping[3], :]
 
-    blue = img[:, :, 0]
-    red = img[:, :, 2]
-    gray_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    claheObj = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(3, 3))
-    clahe = claheObj.apply(gray_image)
-    claheRed = claheObj.apply(red)
-    claheBlue = claheObj.apply(blue)
-
-    return np.stack((claheBlue, claheRed, clahe), axis=2)
+def get_cropped_shape(img_shape, cropping):
+    return (img_shape[0] - cropping[0] - cropping[1],
+            img_shape[1] - cropping[2] - cropping[3],
+            img_shape[2])
 
 
 def apply_clahe(filename, mirrored=False):
-    #image = cv2.imread(INITIAL_ITERATION + filename)
-    image = cv2.imread(filename)
+    image = cv2.imread(INITIAL_ITERATION + filename)
+    # image = cv2.imread(filename)
     image = cv2.resize(image, (WIDTH, HEIGHTS))
+    image = crop_image(image, CROPPING)
     if mirrored:
         image = cv2.flip(image, 1)
 
@@ -73,39 +77,72 @@ def apply_clahe(filename, mirrored=False):
 
 def generate_cnn_model():
     cnn = Sequential()
-    cnn.add(Lambda(lambda x: x / 128. - 1., input_shape=SHAPE, output_shape=SHAPE))
-    cnn.add(Convolution2D(24, 5, 5, subsample=(2, 2), input_shape=SHAPE, W_regularizer=l2(REG), b_regularizer=l2(REG)))
+    cnn.add(Lambda(lambda x: x / 127.5 - 1., input_shape=get_cropped_shape(SHAPE, CROPPING), output_shape=SHAPE))
+    cnn.add(Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2), input_shape=SHAPE))
+    cnn.add(LeakyReLU())
+    cnn.add(MaxPooling2D((2, 2), (1, 1)))
+    cnn.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2, 2)))
+    cnn.add(LeakyReLU())
+    cnn.add(MaxPooling2D((2, 2), (1, 1)))
+    cnn.add(Convolution2D(48, 5, 5, border_mode='same', subsample=(2, 2)))
     cnn.add(Dropout(0.5))
     cnn.add(LeakyReLU())
-    cnn.add(Convolution2D(36, 5, 5, subsample=(2, 2), W_regularizer=l2(REG)))
+    cnn.add(MaxPooling2D((2, 2), (1, 1)))
+    cnn.add(Convolution2D(64, 3, 3))
+    cnn.add(LeakyReLU())
+    cnn.add(MaxPooling2D((2, 2), (1, 1)))
+    cnn.add(Convolution2D(64, 3, 3))
     cnn.add(Dropout(0.5))
     cnn.add(LeakyReLU())
-    cnn.add(Convolution2D(48, 5, 5, subsample=(2, 2), W_regularizer=l2(REG)))
-    cnn.add(Dropout(0.5))
-    cnn.add(LeakyReLU())
-    cnn.add(Convolution2D(64, 3, 3, W_regularizer=l2(REG), b_regularizer=l2(REG)))
-    cnn.add(Dropout(0.5))
-    cnn.add(LeakyReLU())
-    cnn.add(Convolution2D(64, 3, 3, W_regularizer=l2(REG), b_regularizer=l2(REG)))
-    cnn.add(Dropout(0.5))
-    cnn.add(LeakyReLU())
+    cnn.add(MaxPooling2D((2, 2), (1, 1)))
     cnn.add(Flatten())
-    cnn.add(Dense(100, W_regularizer=l2(REG), b_regularizer=l2(REG)))
+    cnn.add(Dense(100))
     cnn.add(LeakyReLU())
-    cnn.add(Dense(50, W_regularizer=l2(REG), b_regularizer=l2(REG)))
+    cnn.add(Dense(50))
     cnn.add(LeakyReLU())
-    cnn.add(Dense(10, W_regularizer=l2(REG), b_regularizer=l2(REG)))
+    cnn.add(Dense(10))
     cnn.add(LeakyReLU())
-    cnn.add(Dense(1, W_regularizer=l2(REG), b_regularizer=l2(REG)))
-
+    cnn.add(Dense(1))
+    cnn.summary()
     return cnn
+
+
+def vgg16_model():
+    input_layer = Input(shape=get_cropped_shape(SHAPE, CROPPING))
+    base_model = VGG16(include_top=False, input_tensor=input_layer)
+
+    # Remove the last block of the VGG16 net.
+    [base_model.layers.pop() for _ in range(4)]
+    base_model.outputs = [base_model.layers[-1].output]
+    base_model.layers[-1].outbound_nodes = []
+
+    # Make sure pre trained layers from the VGG net don't change while training.
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    # Add last block to the VGG model with modified sub sampling.
+    layer = base_model.outputs[0]
+    layer = Convolution2D(512, 3, 3, subsample=(2, 2), activation='relu', border_mode='same', name='block5_conv1')(
+        layer)
+    layer = Convolution2D(512, 3, 3, subsample=(1, 2), activation='relu', border_mode='same', name='block5_conv2')(
+        layer)
+    layer = Convolution2D(512, 3, 3, subsample=(1, 2), activation='relu', border_mode='same', name='block5_conv3')(
+        layer)
+    layer = Dropout(.5)(layer)
+    layer = Flatten()(layer)
+    layer = Dense(2048, activation='relu', name='fc1')(layer)
+    layer = Dense(1024, activation='relu', name='fc2')(layer)
+    layer = Dropout(.5)(layer)
+    layer = Dense(1, activation='linear', name='predictions')(layer)
+
+    return Model(input=base_model.input, output=layer)
 
 
 def generate_data_from_driving(images):
     closed_counter = 0
     while 1:
-        final_images = np.ndarray((BATCH, HEIGHTS, WIDTH, CHANNEL_NUMBER), dtype=float)
-        final_angles = np.ndarray(BATCH, dtype=float)
+        x = np.ndarray((BATCH, *get_cropped_shape(SHAPE, CROPPING)), dtype=float)
+        y = np.ndarray(BATCH, dtype=float)
         for k in range(BATCH):
             if closed_counter == len(images):
                 shuffle(images)
@@ -117,10 +154,10 @@ def generate_data_from_driving(images):
             final_image = apply_clahe(filename, mirrored)
             final_angle = np.ndarray(1, dtype=float)
             final_angle[0] = angle
-            final_images[k] = final_image
-            final_angles[k] = angle
+            x[k] = final_image
+            y[k] = angle
             closed_counter += 1
-        yield ({'lambda_input_1': final_images}, {'dense_4': final_angles})
+        yield ({'lambda_input_1': x}, {'dense_4': y})
 
 
 def epoch_data_length(data_length):
@@ -140,7 +177,7 @@ for i in range(driving_log_length):
 
 driving_log_length = len(X_train)
 
-if not FINE_TUNING:
+if not False:
     for i in range(driving_log_length):
         if X_train[i][1] != 0.0:
             X_train.append([X_train[i][0], -1.0 * X_train[i][1], True])
@@ -166,15 +203,24 @@ if FINE_TUNING:
 else:
     model = generate_cnn_model()
 
-model.compile(Adam(learning_rate), 'mean_squared_error', ['accuracy'])
+model.compile(Adam(learning_rate), 'mse')
+
+checkpoint = ModelCheckpoint('model.h5', 'val_loss', 1, True)
+early_stopping = EarlyStopping('val_loss', patience=5, verbose=1)
+
+train_gen = util.generate_next_batch()
+validation_gen = util.generate_next_batch()
 
 history = model.fit_generator(
     generate_data_from_driving(X_train),
     epoch_data_length(len(X_train)),
     EPOCHS,
     validation_data=generate_data_from_driving(X_valid),
-    nb_val_samples=epoch_data_length(len(X_valid))
+    nb_val_samples=epoch_data_length(len(X_valid)),
+    callbacks=[checkpoint, early_stopping]
 )
+
+# ImageDataGenerator()
 
 accuracy = model.evaluate_generator(generate_data_from_driving(X_test), epoch_data_length(len(X_test)))
 
